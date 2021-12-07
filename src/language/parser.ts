5cfcbf87559adf361f3b5aeafccaca42a0533209
twof <hello@alex.dev>
2021-12-07 15:40:18 -0800

    npm test passes

--- src/language/parser.ts
+++ src/language/parser.ts
@@ -62,7 +62,6 @@ import type {
   NullabilityModifierNode,
   OptionalModifierNode,
   RequiredModifierNode,
-
 } from './ast';
 import { Kind } from './kinds';
 import { Location, OperationTypeNode } from './ast';
@@ -456,7 +455,7 @@ export class Parser {
   parseRequiredModifierNode(): RequiredModifierNode | undefined {
     if (this.expectOptionalToken(TokenKind.BANG)) {
       return this.node<RequiredModifierNode>(this._lexer.token, {
-        kind: Kind.REQUIRED_DESIGNATOR
+        kind: Kind.REQUIRED_DESIGNATOR,
       });
     }
   }
@@ -464,7 +463,7 @@ export class Parser {
   parseOptionalModifierNode(): OptionalModifierNode | undefined {
     if (this.expectOptionalToken(TokenKind.QUESTION_MARK)) {
       return this.node<OptionalModifierNode>(this._lexer.token, {
-        kind: Kind.OPTIONAL_DESIGNATOR
+        kind: Kind.OPTIONAL_DESIGNATOR,
       });
     }
   }
